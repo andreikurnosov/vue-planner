789fc29dc6000da51d361b80341c095e2f0a1d55
andreikurnosov <nixdie@yandex.ru>
2021-05-18 17:15:12 +0700

    using material icons

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -3,7 +3,15 @@
     <div class="actions">
       <h3 @click="showHideDetails">
         {{ project.title }}
+        <span class="material-icons"
+          >{{ showDetails ? 'expand_less' : 'expand_more' }}
+        </span>
       </h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
     </div>
     <div class="details" v-if="showDetails">
       <p>{{ project.details }}</p>
@@ -46,10 +54,27 @@ export default {
   border-left: 4px solid #e90074;
 }
 .details {
-  opacity: .7;
+  opacity: 0.7;
 }
 
 h3 {
   cursor: pointer;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.material-icons {
+  font-size: 2.4rem;
+  margin-left: 1rem;
+  color: #bbb;
+  cursor: pointer;
+}
+
+.material-icons:hover {
+  color: #777;
 }
 </style>
